496553eaca59f59fc42ed9ebd1b337ee07fc4544
Arman <gcom0733@gmail.com>
2023-02-14 15:44:04 +0600

    implemented methods

--- src/main/java/lang/print/gaps/task3/ReassigningValues.java
+++ src/main/java/lang/print/gaps/task3/ReassigningValues.java
@@ -2,6 +2,8 @@ package lang.print.gaps.task3;
 
 public class ReassigningValues {
     public static void main(String[] args) {
-        //write code here
+        int first = 1;
+        int second = 10;
+        int third = 100;
     }
 }
